76cff15126e8f556aeb18d625776009b9c8dbfde
janmonschke <jan.monschke@soundcloud.com>
2015-04-10 08:32:00 +0200

    moar testing

--- app/diffsync.js
+++ app/diffsync.js
@@ -1,10 +1,19 @@
-var _       = require('underscore'),
-  COMMANDS  = {
-    syncWithServer: 'send-edit',
-    getInitialVersion: 'get-latest-document-version',
-    remoteUpdateIncoming: 'updated-doc'
-  },
-  Client;
+var _             = require('underscore'),
+    jsondiffpatch = require('jsondiffpatch').create({
+      objectHash: function(obj) { return obj.id || obj._id || JSON.stringify(obj); }
+    }),
+    COMMANDS      = {
+      syncWithServer: 'send-edit',
+      getInitialVersion: 'get-latest-document-version',
+      remoteUpdateIncoming: 'updated-doc'
+    },
+    utils, Client;
+
+utils = {
+  deepCopy: function(obj){
+    return JSON.parse(JSON.stringify(obj));
+  }
+};
 
 Client = function(socket, room){
   if(!socket){ throw new Error('No socket specified'); }
@@ -20,21 +29,198 @@ Client = function(socket, room){
     serverVersion: 0,
     shadow: {},
     localCopy: {},
-    edits: []
+    diffs: []
   };
 
-  _.bindAll(this, '_onConnected');
+  _.bindAll(this, '_onConnected', 'syncWithServer', 'applyServerEdit');
 };
 
+/**
+ * Initializes the sync session
+ * @return {[type]} [description]
+ */
 Client.prototype.initialize = function(){
   // connect, join room and initialize
+  this.syncing = true;
   this.socket.emit('join', this.room, this._onConnected);
 };
 
-Client.prototype._onConnected = function(){
-  this.socket.emit('ack', { data: [1, 2, 3], room: this.room });
+/**
+ * Sets up the local version and listens to server updates
+ * Will notify the `onConnected` callback.
+ * @param  {Object} initialVersion The initial version from the server
+ */
+Client.prototype._onConnected = function(initialVersion){
+  // client is not syncing anymore and is initialized
+  this.syncing = false;
+  this.initialized = true;
+
+  // set up shadow doc, local doc and initial server version
+  // IMPORTANT: the shadow needs to be a deep copy of the initial version
+  // because otherwise changes to the local object will also result in changes
+  // to the shadow object because they are pointing to the same doc
+  this.doc.shadow = utils.deepCopy(initialVersion);
+  this.doc.localCopy = initialVersion;
+  this.doc.serverVersion = 0;
+
+  // listen to incoming updates from the server
+  this.socket.on(COMMANDS.remoteUpdateIncoming, this.schedule);
+
+  // notify about established connection
+  this.onConnected();
 };
 
+/**
+ * Schedule a sync cycle. This method should be used from the outside to
+ * trigger syncs.
+ */
+Client.prototype.schedule = function(){
+  // do nothing if already scheduled
+  if(this.scheduled){ return; }
+  this.scheduled = true;
+
+  // try to sync now
+  this.syncWithServer();
+};
+
+/**
+ * Starts a sync cycle. Should not be called from third parties
+ */
+Client.prototype.syncWithServer = function(){
+  if(this.syncing || !this.initialized){ return false; }
+  if(this.scheduled){ this.scheduled = false; }
+
+  // initiate syncing cycle
+  this.syncing = true;
+
+  // 1) create a diff of local copy and shadow
+  var diff = this.createDiff(this.doc.shadow, this.doc.localCopy);
+  var basedOnLocalVersion = this.doc.localVersion;
+
+  // 2) add the difference to the local edits stack if the diff is not empty
+  if(!_.isEmpty(diff)){
+    this.doc.diffs.push(this.createDiffMessage(diff, basedOnLocalVersion));
+    this.doc.localVersion++;
+  }
+
+  // 3) create an edit message with all relevant version numbers
+  var editMessage = this.createEditMessage(basedOnLocalVersion);
+
+  // 4) apply the patch to the local shadow
+  this.applyPatchTo(this.doc.shadow, utils.deepCopy(diff));
+
+  // 5) send the edits to the server
+  this.sendEdits(editMessage);
+
+  // yes, we're syncing
+  return true;
+};
+
+/**
+ * Returns a diff of the passed documents
+ * @param  {Object} docA
+ * @param  {Object} docB
+ * @return {Diff}      The diff of both documents
+ */
+Client.prototype.createDiff = function(docA, docB){
+  return jsondiffpatch.diff(docA, docB);
+};
+
+/**
+ * Applies the path to the specified object
+ * WARNING: The patch is applied in place!
+ * @param  {Object} obj
+ * @param  {Diff} patch
+ */
+Client.prototype.applyPatchTo = function(obj, patch){
+  jsondiffpatch.patch(obj, patch);
+};
+
+/**
+ * Creates a message for the specified diff
+ * @param  {Diff} diff          the diff that will be sent
+ * @param  {Number} baseVersion the version of which the diff is based
+ * @return {Object}             a diff message
+ */
+Client.prototype.createDiffMessage = function(diff, baseVersion){
+  return {
+    serverVersion: this.doc.serverVersion,
+    localVersion: baseVersion,
+    diff: diff
+  };
+};
+
+/**
+ * Creates a message representing a set of edits
+ * An edit message contains all edits since the last sync has happened.
+ * @param  {Number} baseVersion The version that these edits are based on
+ * @return {Object}             An edit message
+ */
+Client.prototype.createEditMessage = function(baseVersion){
+  return {
+    room: this.room,
+    edits: this.doc.edits,
+    localVersion: baseVersion,
+    serverVersion: this.doc.serverVersion
+  };
+};
+
+/**
+ * Send the the edits to the server and applies potential updates from the server
+ */
+Client.prototype.sendEdits = function(editMessage){
+  this.socket.emit(COMMANDS.syncWithServer, editMessage, this.applyServerEdits);
+};
+
+Client.prototype.applyServerEdits = _.noop;
+// applyServerEdits: function(serverEdits){
+//   if(serverEdits && serverEdits.localVersion == this.doc.localVersion){
+//     // 0) delete all previous edits
+//     this.doc.edits = [];
+//     // 1) iterate over all edits
+//     serverEdits.edits.forEach(this.applyServerEdit.bind(this));
+//   }else{
+//     console.log('rejected patch because localVersions don\'t match');
+//   }
+
+//   this.syncing = false;
+//   this.scheduled = false;
+// },
+
+
+/**
+ * Applies a single edit message to the local copy and the shadow
+ * @param  {[type]} editMessage [description]
+ * @return {[type]}             [description]
+ */
+Client.prototype.applyServerEdit =  function(editMessage){
+  // 2) check the version numbers
+  if(editMessage.localVersion === this.doc.localVersion &&
+    editMessage.serverVersion === this.doc.serverVersion){
+
+    // versions match
+    // 3) patch the shadow
+    this.applyPatchTo(this.doc.shadow, editMessage.diff);
+
+    // 4) increase the version number for the shadow if diff not empty
+    if(!_.isEmpty(editMessage.diff)){
+      this.doc.serverVersion++;
+    }
+    // apply the patch to the local document
+    // IMPORTANT: Use a copy of the diff, or newly created objects will be copied by reference!
+    this.applyPatchTo(this.doc.localCopy, utils.deepCopy(editMessage.diff));
+
+    return true;
+  }else{
+    // TODO: check in the algo paper what should happen in the case of not mathing version numbers
+    return false;
+  }
+};
+
+Client.prototype.onConnected = _.noop;
+Client.prototype.onSynced = _.noop;
+
 module.exports = {
-  Client: Client
+  Client: Client,
+  COMMANDS: COMMANDS
 };
--- test/diffsync/client_test.js
+++ test/diffsync/client_test.js
@@ -1,6 +1,9 @@
-var assert = require('assert'),
-    sinon  = require('sinon'),
-    Client = require('../../app/diffSync').Client;
+var assert        = require('assert'),
+    sinon         = require('sinon'),
+    _             = require('underscore'),
+    jsondiffpatch = require('jsondiffpatch'),
+    COMMANDS      = require('../../app/diffSync').COMMANDS,
+    Client        = require('../../app/diffSync').Client;
 
 describe('DiffSync Client', function(){
 
@@ -9,6 +12,10 @@ describe('DiffSync Client', function(){
       emit: function(){},
       on: function(){}
     }, 'testroom');
+  };
+
+  var testData = function(){
+    return {a: 1, b: [{c: 1}]};
   };
 
   describe('constructor', function(){
@@ -37,6 +44,215 @@ describe('DiffSync Client', function(){
 
       assert(spy.called);
       assert(spy.calledWith('join', c.room));
+    });
+  });
+
+  describe('_onConnected', function(){
+    var client;
+    beforeEach(function(){
+      client = testClient();
+    });
+
+    it('should set the model in initialized state', function(){
+      assert(!client.initialized);
+      client._onConnected({});
+      assert(client.initialized);
+    });
+
+    it('should release the sync cycle', function(){
+      client.initialize();
+      assert(client.syncing);
+      client._onConnected({});
+      assert(!client.syncing);
+    });
+
+    it('should subscribe to server sync requests', function(){
+      var spy = sinon.spy(client.socket, 'on');
+
+      client._onConnected({});
+      assert(spy.calledWith(COMMANDS.remoteUpdateIncoming, client.schedule));
+    });
+
+    it('should set the shadow and the local copy correctly', function(){
+      client._onConnected({ test: true, arr: [{a: 1}] });
+      assert.deepEqual(client.doc.localCopy, client.doc.shadow, 'both versions should be identical by value');
+      assert.notStrictEqual(client.doc.localCopy, client.doc.shadow, 'they shouldnt be the same reference');
+    });
+
+    it('should trigger the `onConnected` callback', function(){
+      var spy = sinon.spy(client, 'onConnected');
+
+      client._onConnected({});
+      assert(spy.calledOnce);
+    });
+  });
+
+  describe('schedule', function(){
+    var client;
+    beforeEach(function(){
+      client = testClient();
+    });
+
+    it('should schedule a sync', function(){
+      assert(!client.scheduled);
+      client.schedule();
+      assert(client.scheduled);
+    });
+
+    it('should try to sync', function(){
+      var spy = sinon.spy(client, 'syncWithServer');
+      client.schedule();
+      assert(spy.calledOnce);
+    });
+  });
+
+  describe('createDiff', function(){
+    it('should create an empty diff for equal objects', function(){
+      var a = {test: true};
+      var b = {test: true};
+      var diff = testClient().createDiff(a, b);
+
+      assert(_.isEmpty(diff));
+    });
+
+    it('should create an not empty diff for equal objects', function(){
+      var a = {test: true, test2: true};
+      var b = {test: true};
+      var diff = testClient().createDiff(a, b);
+
+      assert(!_.isEmpty(diff));
+    });
+  });
+
+  describe('createDiffMessage', function(){
+    it('should create a valid diff object', function(){
+      var client        = testClient(),
+          serverVersion = client.doc.serverVersion,
+          diff          = {},
+          baseVersion   = 1,
+          diffMessage   = client.createDiffMessage(diff, baseVersion);
+
+      assert.strictEqual(diffMessage.serverVersion, serverVersion);
+      assert.strictEqual(diffMessage.localVersion, baseVersion);
+      assert.strictEqual(diffMessage.diff, diff);
+    });
+  });
+
+
+  describe('createEditMessage', function(){
+    it('should create a valid edit message', function(){
+      var client        = testClient(),
+          baseVersion   = 1,
+          editMessage   = client.createEditMessage(baseVersion);
+
+      assert.equal(editMessage.room, client.room);
+      assert.equal(editMessage.localVersion, baseVersion);
+      assert.equal(editMessage.serverVersion, client.doc.serverVersion);
+      assert.equal(editMessage.edits, client.edits);
+    });
+  });
+
+  describe('syncWithServer', function(){
+    var client, data, changeLocalDoc;
+    beforeEach(function(){
+      data = testData();
+      client = testClient();
+      client._onConnected(data);
+    });
+
+    changeLocalDoc = function(){
+      client.doc.localCopy.b[0].c = 2;
+    };
+
+    it('should not sync if not initalized', function(){
+      client.initialized = false;
+      assert.equal(false, client.syncWithServer());
+    });
+
+    it('should not sync if currently syncing', function(){
+      client.syncing = true;
+      assert.equal(false, client.syncWithServer());
+    });
+
+    it('should reset the scheduled flag', function(){
+      client.scheduled = true;
+      changeLocalDoc();
+      client.syncWithServer();
+      assert.equal(false, client.scheduled);
+    });
+
+    it('should set syncing flag', function(){
+      assert(!client.syncing);
+      changeLocalDoc();
+      client.syncWithServer();
+      assert(client.syncing);
+    });
+
+    it('should perform a valid client-sync circle init', function(){
+      var createDiff = sinon.spy(client, 'createDiff'),
+          createDiffMessage = sinon.spy(client, 'createDiffMessage'),
+          createEditMessage = sinon.spy(client, 'createEditMessage'),
+          applyPatchTo = sinon.spy(client, 'applyPatchTo'),
+          sendEdits = sinon.spy(client, 'sendEdits'),
+          localVersionBeforeChange = client.doc.localVersion;
+
+      // assert correct version
+      assert.equal(client.doc.localVersion, 0, 'initial version number is 0');
+
+      // change local version
+      client.doc.localCopy.b[0].c = 2;
+      client.syncWithServer();
+
+      // creates a diff from shadow and local copy
+      assert(createDiff.called, 'calls createDiff');
+      assert(createDiff.calledWithExactly(client.doc.shadow, client.doc.localCopy), 'createDiff called with correct objects');
+
+      // creates a diff message from that diff
+      assert(createDiffMessage.calledAfter(createDiff), 'calls createDiffMessage after createDiff');
+
+      // creates and edit message from that diff with correct local version
+      assert(createEditMessage.calledAfter(createDiffMessage), 'calls createEditMessage after createDiffMessage');
+      assert(createEditMessage.calledWithExactly(localVersionBeforeChange), 'createEditMessage is called with correct local version from before the change');
+
+      // applies patch to shadow
+      assert(applyPatchTo.calledAfter(createEditMessage), 'calls applyPatchTo after createEditMessage');
+      assert.deepEqual(client.doc.shadow, client.doc.localCopy, 'applyPatchTo creates deep equality');
+
+      assert.notStrictEqual(client.doc.shadow, client.doc.localCopy, 'shadow and local copy are equal, but not same references');
+      assert.notStrictEqual(client.doc.shadow.b, client.doc.localCopy.b, 'shadow and local copy are equal, but not same references');
+      assert.notStrictEqual(client.doc.shadow.b[0], client.doc.localCopy.b[0], 'shadow and local copy are equal, but not same references');
+
+      // send the edits to the server
+      assert(sendEdits.calledAfter(applyPatchTo), 'calls sendEdits after applyPatchTo');
+
+      // assert correctly updated local version number
+      assert.equal(client.doc.localVersion, 1, 'updated version number is 1');
+    });
+  });
+
+  describe('applyServerEdit', function(){
+    var client, edit, diff, serverData;
+
+    beforeEach(function(){
+      client = testClient();
+      client._onConnected(testData());
+      serverData = testData();
+      serverData.b[0].c = 2;
+
+      diff = JSON.parse(JSON.stringify(jsondiffpatch.diff(client.doc.localCopy, serverData)));
+      edit = {
+        localVersion: client.doc.localVersion,
+        serverVersion: client.doc.serverVersion,
+        diff: diff
+      };
+    });
+
+    it('should apply the server changes and copy all values', function(){
+      assert.notEqual(client.doc.localCopy.b[0].c, serverData.b[0].c, 'local version and remote version differ');
+      client.applyServerEdit(edit);
+      assert.equal(client.doc.localCopy.b[0].c, serverData.b[0].c, 'local version and remote version are equal');
+      assert.deepEqual(client.doc.localCopy, client.doc.shadow, 'local version and shadow version are deep equal');
+      assert.deepEqual(client.doc.localCopy.b[0], client.doc.shadow.b[0], 'local version and shadow version are not the same references');
     });
   });
 
